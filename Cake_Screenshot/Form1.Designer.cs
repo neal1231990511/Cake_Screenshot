4c1f5cce7b5dba7b1aa95c1aebd46fa1f760f9c8
KentLin <snowcat_829@hotmail.com>
2015-11-10 14:44:46 +0800

    xxxx1

--- Cake_Screenshot/Form1.Designer.cs
+++ Cake_Screenshot/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.ptbPhotoPreview)).BeginInit();
             this.SuspendLayout();
@@ -60,7 +61,7 @@
             this.btnScreenshot.BackColor = System.Drawing.SystemColors.Control;
             this.btnScreenshot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.btnScreenshot.FlatStyle = System.Windows.Forms.FlatStyle.System;
-            this.btnScreenshot.Font = new System.Drawing.Font("Segoe Marker", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScreenshot.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnScreenshot.ForeColor = System.Drawing.Color.SteelBlue;
             this.btnScreenshot.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
             this.btnScreenshot.Location = new System.Drawing.Point(441, 681);
@@ -112,33 +113,42 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe Marker", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.Location = new System.Drawing.Point(253, 655);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(69, 22);
+            this.label2.Size = new System.Drawing.Size(84, 24);
             this.label2.TabIndex = 7;
             this.label2.Text = "Devices";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Segoe Marker", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(491, 655);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(59, 22);
+            this.label3.Size = new System.Drawing.Size(66, 24);
             this.label3.TabIndex = 8;
             this.label3.Text = "Ctrl+B";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe Marker", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Red;
             this.label4.Location = new System.Drawing.Point(672, 825);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(82, 30);
+            this.label4.Size = new System.Drawing.Size(85, 32);
             this.label4.TabIndex = 9;
             this.label4.Text = "Ctrl + B 拍照\r\nCtrl  + V 貼上\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 87);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -146,6 +156,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.ClientSize = new System.Drawing.Size(820, 873);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -180,6 +191,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
 
